a4de6866fa55dfdc6448b08f31b8fa28fff5603f
JazzJayDeve <jsemongreen@gmail.com>
2018-01-24 21:40:02 -0500

    yes

---
+++ Javascript prework/javascript.js
@@ -0,0 +1,12 @@
+$("#button1").on("click", function() {
+	// Increase the size of the box.
+	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
+})
+$("#button2").on("click", function() {
+	// Increase the size of the box.
+	$("#box").animate({"color","blue"},"fast");
+})
+$("#button4").on("click", function() {
+	// Increase the size of the box.
+	$("#box").animate({height:150px; width:150px; background-color:orange; margin:25px}, "fast");
+})
